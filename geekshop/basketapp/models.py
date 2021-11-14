79fb7bbf80a86d4e933c16c259ef3c9cb383ae87
vlgreb <vlgrebennikov@mail.ru>
2021-11-14 19:15:13 +0300

    'hw6

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -11,3 +11,18 @@ class Basket(models.Model):
 
     add_datetime = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def product_cost(self):
+        return self.quantity * self.product.price
+
+    @property
+    def total_quantity(self):
+        _items = Basket.objects.filter(user=self.user)
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+    @property
+    def total_cost(self):
+        _items = Basket.objects.filter(user=self.user)
+        return sum(list(map(lambda x: x.product_cost, _items)))
+
--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,13 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 from basketapp.models import Basket
 from mainapp.models import Product
 
 
+@login_required
 def basket(request):
-    pass
+    baskets_list = Basket.objects.filter(user=request.user)
+
+    context = {
+        'baskets': baskets_list
+    }
+
+    return render(request, 'basketapp/basket.html', context)
 
 
+@login_required
 def add(request, pk):
     product_item = get_object_or_404(Product, pk=pk)
 
@@ -22,7 +33,31 @@ def add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def remove(request, pk):
     basket_item = get_object_or_404(Basket, pk=pk)
     basket_item.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        basket_item = Basket.objects.get(pk=pk)
+
+        if quantity > 0:
+            basket_item.quantity = quantity
+            basket_item.save()
+        else:
+            basket_item.delete()
+
+        basket_list = Basket.objects.filter(user=request.user)
+
+        context = {
+            'baskets': basket_list
+        }
+
+        result = render_to_string('basketapp/includes/inc_baskets_list.html', context)
+
+        return JsonResponse({'result': result})
